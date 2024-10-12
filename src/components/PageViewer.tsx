992f4db731abe9aedacaf105bc4d98e89e02f13a
satyam_chaudhary <satyam.chaudhary@brightchamps.com>
2024-10-12 21:34:58 +0530

    added animation

--- src/components/PageViewer.tsx
+++ src/components/PageViewer.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { Page } from "../type/type";
 
 interface PageViewerProps {
@@ -9,41 +10,80 @@ interface PageViewerProps {
   onPreviousChapter: () => void;
 }
 
-const PageViewer: React.FC<PageViewerProps> = React.memo(({
+export const PageViewer: React.FC<PageViewerProps> = React.memo(({
   pages,
   currentPageIndex,
   onPageChange,
   onNextChapter,
   onPreviousChapter,
 }) => {
-  const handlePageClick = (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+  const [direction, setDirection] = useState<number>(0);
+
+  const variants = {
+    enter: (direction: number) => ({
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    }),
+    center: {
+      x: 0,
+      opacity: 1,
+    },
+    exit: (direction: number) => ({
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+    }),
+  };
+
+  const handlePageClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const screenWidth = window.innerWidth;
     const clickPosition = e.clientX;
 
     if (clickPosition < screenWidth / 2) {
       // Left side clicked (Previous page)
       if (currentPageIndex > 0) {
+        setDirection(-1);
         onPageChange(currentPageIndex - 1);
       } else {
+        setDirection(-1);
         onPreviousChapter();
       }
     } else {
       // Right side clicked (Next page)
       if (currentPageIndex < pages.length - 1) {
+        setDirection(1);
         onPageChange(currentPageIndex + 1);
       } else {
+        setDirection(1);
         onNextChapter();
       }
     }
   };
 
   return (
-    <img
-      src={pages[currentPageIndex]?.image?.file}
-      alt={`Page ${currentPageIndex + 1}`}
-      onClick={handlePageClick}
-      style={{ cursor: "pointer", width: "100%" }}
-    />
+    <div className="page-viewer" onClick={handlePageClick} style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }}>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={currentPageIndex}
+          src={pages[currentPageIndex]?.image?.file}
+          alt={`Page ${currentPageIndex + 1}`}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 },
+          }}
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            objectFit: 'contain',
+          }}
+        />
+      </AnimatePresence>
+    </div>
   );
 });
 
